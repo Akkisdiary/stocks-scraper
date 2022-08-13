c1df2281034d384bfa6c373a99719c95a4415653
Akshay Shegaonkar <shegaonkar.akshay2@gmail.com>
2022-08-13 16:29:10 +0530

    add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 
 from .api import create_api
 
 
 def create_app():
     app = Flask(__name__)
+
+    cors = CORS()
+    cors.init_app(app)
 
     api = create_api()
     api.init_app(app)
